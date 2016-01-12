e3cb990f021840530bdac71b0eb2d2bc1a061f58
Nicolas Thal <nico.th4l@gmail.com>
2016-01-12 15:06:17 +0100

    add commands

---
+++ ElasticaBundle/Command/OrchestraCreateSchemaCommand.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace OpenOrchestra\ElasticaBundle\Command;
+
+use LogicException;
+use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+/**
+ * Class OrchestraCreateSchemaCommand
+ */
+class OrchestraCreateSchemaCommand extends ContainerAwareCommand
+{
+    /**
+     * Configures the current command.
+     */
+    protected function configure()
+    {
+        $this
+            ->setName('orchestra:elastica:schema:create')
+            ->setDescription('Load the schema from the content types');
+    }
+
+    /**
+     * @param InputInterface $input An InputInterface instance
+     * @param OutputInterface $output An OutputInterface instance
+     *
+     * @return null|int null or 0 if everything went fine, or an error code
+     *
+     * @throws LogicException When this abstract method is not implemented
+     */
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $contentTypes = $this->getContainer()->get('open_orchestra_model.repository.content_type')->findAllNotDeletedInLastVersion();
+
+        foreach ($contentTypes as $contentType) {
+            $this->getContainer()->get('open_orchestra_elastica.schema_generator.content_type')->createMaping($contentType);
+        }
+    }
+}
