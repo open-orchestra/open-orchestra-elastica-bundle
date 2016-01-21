34908e815ca04252fb70d329776f50fff1369fdd
Nicolas Thal <nico.th4l@gmail.com>
2016-01-21 16:05:22 +0100

    add display icon strategy

---
+++ ElasticaAdmin/DisplayIcon/ElasticaSearchStrategy.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace OpenOrchestra\ElasticaAdmin\DisplayIcon;
+
+use OpenOrchestra\BackofficeBundle\DisplayIcon\Strategies\AbstractStrategy;
+
+/**
+ * Class ElasticaSearchStrategy
+ */
+class ElasticaSearchStrategy extends AbstractStrategy
+{
+    /**
+     * Check if the strategy support this block
+     *
+     * @param string $block
+     *
+     * @return boolean
+     */
+    public function support($block)
+    {
+        return 'elastica_search' == $block;
+    }
+
+    /**
+     * Perform the show action for a block
+     *
+     * @return string
+     */
+    public function show()
+    {
+        return $this->render('OpenOrchestraElasticaAdminBundle:Block/Search:showIcon.html.twig');
+    }
+
+    /**
+     * Get the name of the strategy
+     *
+     * @return string
+     */
+    public function getName()
+    {
+        return 'elastica_search';
+    }
+}
