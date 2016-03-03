5f547ba7fee367b446dd63853cea52f23a200134
itkg-ngilain <noel.gilain@businessdecision.com>
2016-03-03 10:19:08 +0100

    Move getCacheTags from abstract strategy to elastica strategies

--- ElasticaFront/DisplayBlock/ElasticaSearchStrategy.php
+++ ElasticaFront/DisplayBlock/ElasticaSearchStrategy.php
@@ -59,6 +59,16 @@ class ElasticaSearchStrategy extends AbstractStrategy
     }
 
     /**
+     * @param ReadBlockInterface $block
+     *
+     * @return Array
+     */
+    public function getCacheTags(ReadBlockInterface $block)
+    {
+        return array();
+    }
+
+    /**
      * Get the name of the strategy
      *
      * @return string
