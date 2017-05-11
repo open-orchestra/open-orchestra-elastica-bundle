bbbd27e9b4e3f7e09b35e748d86b2b82028e071a
Amaury Lavieille <amaury.lavieille@businessdecision.com>
2017-05-11 15:28:09 +0200

    move method elastica

--- Elastica/Transformer/NodeTransformer.php
+++ Elastica/Transformer/NodeTransformer.php
@@ -3,7 +3,7 @@
 namespace OpenOrchestra\Elastica\Transformer;
 
 use Elastica\Document;
-use OpenOrchestra\DisplayBundle\DisplayBlock\DisplayBlockManager;
+use OpenOrchestra\BackOffice\DisplayBlock\DisplayBlockManager;
 use OpenOrchestra\ModelInterface\Model\ReadBlockInterface;
 use OpenOrchestra\ModelInterface\Model\ReadNodeInterface;
 
